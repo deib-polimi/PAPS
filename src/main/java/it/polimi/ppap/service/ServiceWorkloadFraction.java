1c2cc6328c2a38510e8e67124366e4fbb9b2a112
Danilo Filgueira Mendonça <dfmendonca@gmail.com>
2019-05-29 16:12:26 +0200

    fixes workload fluctuation

--- src/main/java/it/polimi/ppap/service/ServiceWorkloadFraction.java
+++ src/main/java/it/polimi/ppap/service/ServiceWorkloadFraction.java
@@ -3,14 +3,21 @@ package it.polimi.ppap.service;
 public class ServiceWorkloadFraction extends ServiceWorkload{
 
     final float workloadFraction;
+    ServiceWorkload serviceWorkload;
 
     public ServiceWorkloadFraction(ServiceWorkload serviceWorkload, float workloadFraction) {
         super(serviceWorkload.getSource(), serviceWorkload.getService(), serviceWorkload.getWorkload(), serviceWorkload.getWorkload());
+        this.serviceWorkload = serviceWorkload;
         this.workloadFraction = workloadFraction;
     }
 
     @Override
     public float getWorkload() {
-        return super.getWorkload() * (1 / workloadFraction);
+        return serviceWorkload.getWorkload() * (1 / workloadFraction);
+    }
+
+    @Override
+    public float getReference() {
+        return serviceWorkload.getReference();
     }
 }
